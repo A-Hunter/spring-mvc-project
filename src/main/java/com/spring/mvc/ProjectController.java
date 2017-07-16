04ad772b040722686ece748a8f96fb5d70eba67d
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2017-07-16 19:08:23 +0100

    @RequestMapping

--- src/main/java/com/spring/mvc/ProjectController.java
+++ src/main/java/com/spring/mvc/ProjectController.java
@@ -2,6 +2,7 @@ package com.spring.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/project")
@@ -11,4 +12,23 @@ public class ProjectController {
 	public String addProject(){
 		return "add_project";
 	}
+
+	@RequestMapping(value="/add", method= RequestMethod.POST)
+	public String saveProject(){
+		System.out.println("invoking saveProject");
+		return "add_project";
+	}
+
+	@RequestMapping(value="/add", method=RequestMethod.POST, params={"type=multi"})
+	public String saveMultiYearProject(){
+		System.out.println("invoking saveMultiYearProject");
+		return "add_project";
+	}
+
+	@RequestMapping(value="/add", method=RequestMethod.POST, params={"type=multi","special"})
+	public String saveSpecialProject(){
+		System.out.println("invoking saveSpecialProject");
+		return "add_project";
+	}
+
 }
